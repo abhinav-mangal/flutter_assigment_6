5da28dbc0bf5f747b4533d17de83dfe7057912b7
abhinav-mangal <68347344+abhinav-mangal@users.noreply.github.com>
2021-05-02 17:58:02 +0530

    Final Commit

--- lib/Pages/EmpListPage.dart
+++ lib/Pages/EmpListPage.dart
@@ -4,8 +4,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_assignment_6/Pages/EmpDetailPage.dart';
 
-// import 'DetailPage.dart';
-
 class EmpListPage extends StatefulWidget {
   EmpListPage({Key key, this.title}) : super(key: key);
 
@@ -36,48 +34,48 @@ class _EmpListPageState extends State<EmpListPage> {
                     .toString()
                     .compareTo(b['lastName'].toLowerCase().toString());
               });
-              return ListView.separated(
-                  separatorBuilder: (context, index) => Divider(
-                        color: Colors.black45,
-                      ),
+              return ListView.builder(
                   itemCount: snapshot.data.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                      title: Padding(
-                        padding: const EdgeInsets.all(0.0),
-                        child: Text(
+                    return Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      elevation: 10,
+                      shadowColor: Colors.blue,
+                      child: ListTile(
+                        title: Text(
                           data[index]['firstName'] +
                               ' ' +
                               data[index]['lastName'],
                           style: TextStyle(
                               color: Colors.blue, fontWeight: FontWeight.w800),
                         ),
+                        subtitle: Text(
+                          data[index]['email'],
+                          style: TextStyle(
+                              color: Colors.blue[300],
+                              fontWeight: FontWeight.w400),
+                        ),
+                        leading: Image.network(
+                          data[index]['imageUrl'],
+                          fit: BoxFit.cover,
+                          height: 40.0,
+                          width: 40.0,
+                        ),
+                        trailing: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.blue[400],
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      DetailPage(data[index])));
+                        },
                       ),
-                      subtitle: Text(
-                        data[index]['email'],
-                        style: TextStyle(
-                            color: Colors.blue[300],
-                            fontWeight: FontWeight.w400),
-                      ),
-                      leading: Image.network(
-                        data[index]['imageUrl'],
-                        fit: BoxFit.cover,
-                        height: 40.0,
-                        width: 40.0,
-                      ),
-                      trailing: Icon(
-                        Icons.arrow_forward_ios,
-                        color: Colors.blue[400],
-                      ),
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => DetailPage(data[index])));
-                      },
                     );
                   });
-              // );
             } else
               return Center(child: CircularProgressIndicator.adaptive());
           },
--- lib/Services/EmployeesData.dart
+++ lib/Services/EmployeesData.dart
@@ -1,7 +1,3 @@
-// To parse this JSON data, do
-//
-//     final employees = employeesFromJson(jsonString);
-
 import 'dart:convert';
 
 List<Employees> employeesFromJson(String str) =>
